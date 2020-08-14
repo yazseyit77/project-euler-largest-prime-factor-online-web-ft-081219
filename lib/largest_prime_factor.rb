d49fe1833f86af1a919faf16ca018cd7df10a08c
Yazgeldi Seyidov <yazgeldi.seyidov@gmail.com>
2020-08-14 19:11:22 +0000

    Automatically backed up by Learn

--- lib/largest_prime_factor.rb
+++ lib/largest_prime_factor.rb
@@ -4,7 +4,7 @@ def largest_prime_factor(input)
   prime = input
   (2...Math.sqrt(input.to_i)).each do |n|
     break if prime <= 1
-    prime ?= n while (prime > n && prime % n == 0)
+    prime /= n while (prime > n && prime % n == 0)
   end
   prime
 end
