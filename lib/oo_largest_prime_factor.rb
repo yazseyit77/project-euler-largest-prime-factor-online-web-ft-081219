abccb9ca419616ee1a10b16122793c95102fe40d
Yazgeldi Seyidov <yazgeldi.seyidov@gmail.com>
2020-08-14 19:12:47 +0000

    Automatically backed up by Learn

--- lib/oo_largest_prime_factor.rb
+++ lib/oo_largest_prime_factor.rb
@@ -1,6 +1,15 @@
 # Enter your object-oriented solution here!
 
 class LargestPrimeFactor
+  attr_reader :input, :number
 
+  def initialize(input)
+    @input = input
+    @number = self.largest_prime_factor()
+  end
+
+  def largest_prime_factor
+
+  end
 
 end
